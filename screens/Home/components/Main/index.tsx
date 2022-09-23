d1cef58190362a238fe7520985e12feb6955e8e9
Hiago <hiagolaureano57@gmail.com>
2022-09-23 11:33:22 -0300

    finalização da tela home

--- screens/Home/components/Main/index.tsx
+++ screens/Home/components/Main/index.tsx
@@ -3,7 +3,7 @@ import { AntDesign } from "@expo/vector-icons";
 
 export default function Main(props:any){
     return(
-        <View style={{flex:2, padding:10, backgroundColor:"#E2E0E0"}}>
+        <View style={{flex:5, padding:10, backgroundColor:"#E2E0E0"}}>
             <TouchableOpacity onPress={()=>props.tela.navigate("Details")}>               
             <View style={{height:100, width:390, backgroundColor:"white",padding:10,borderRadius:20, flexDirection:"row",marginBottom:10}}>
                 <Image source={require("../../../../assets/iconHouse.png" )}style={{width:70, height:70}}/>
@@ -29,10 +29,32 @@ export default function Main(props:any){
             </View>
 
             <View style={{height:100, width:390, backgroundColor:"white",padding:10,borderRadius:20, flexDirection:"row",marginBottom:10}}>
-                <Image source={require("../../../../assets/iconHouse.png" )}style={{width:70, height:70}}/>
+                <Image source={require("../../../../assets/hallPet.png" )}style={{width:70, height:70}}/>
                 <View style={{alignItems:"center"}}>
-                    <Text style={{marginTop:20, marginLeft:30, fontSize:18}}>UM LAR PARA PETS</Text>
-                    <Text style={{marginLeft:30,color:"silver", fontSize:10}}>Veja os locais mais proximos {'\n'} para levar pet abandonado</Text>
+                    <Text style={{marginTop:20, marginLeft:30, fontSize:18}}>HALL PETS ADOTADOS</Text>
+                    <Text style={{marginLeft:30,color:"silver", fontSize:10}}>Veja os pets que ja foram adotados</Text>
+                </View>
+                <AntDesign style={{alignItems:"center",marginTop:30,marginLeft:50}}>
+                <AntDesign name="right" size={25} color="blue"/>
+                </AntDesign>
+            </View>
+
+            <View style={{height:100, width:390, backgroundColor:"white",padding:10,borderRadius:20, flexDirection:"row",marginBottom:10}}>
+                <Image source={require("../../../../assets/userDog.png" )}style={{width:70, height:80}}/>
+                <View style={{alignItems:"center"}}>
+                    <Text style={{marginTop:20, marginLeft:30, fontSize:18}}>CONTATO</Text>
+                    <Text style={{marginLeft:30,color:"silver", fontSize:10}}>Veja os pets que ja foram adotados</Text>
+                </View>
+                <AntDesign style={{alignItems:"center",marginTop:30,marginLeft:80}}>
+                <AntDesign name="right" size={25} color="blue"/>
+                </AntDesign>
+            </View>
+
+            <View style={{height:100, width:390, backgroundColor:"white",padding:10,borderRadius:20, flexDirection:"row",marginBottom:10}}>
+                <Image source={require("../../../../assets/sobreNos.png" )}style={{marginTop:10,width:70, height:60}}/>
+                <View style={{alignItems:"center"}}>
+                    <Text style={{marginTop:20, marginLeft:30, fontSize:18}}>SOBRE NÓS</Text>
+                    <Text style={{marginLeft:30,color:"silver", fontSize:10}}>Saiba mais sobre a nossa historia</Text>
                 </View>
                 <AntDesign style={{alignItems:"center",marginTop:30,marginLeft:80}}>
                 <AntDesign name="right" size={25} color="blue"/>
--- screens/Home/components/Panel/index.tsx
+++ screens/Home/components/Panel/index.tsx
@@ -2,7 +2,7 @@ import { View, Image } from "react-native";
 
 export default function Panel(){
     return(
-        <View style={{flex:1,}}>
+        <View style={{flex:1}}>
              <Image source={require("../../../../assets/panel.jpg")} style={{width:"100%", height:90}}/>
         </View>
     )
