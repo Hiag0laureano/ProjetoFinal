7e7348d28fef9077b4a9d83d510b1f722eded318
GUILHERME FRANCISCO DA SILVA <guilherme.fsilva67@senacsp.edu.br>
2022-09-28 08:25:37 -0300

    mudanas na tela sobrenos

--- screens/SobreNos/components/Main/index.tsx
+++ screens/SobreNos/components/Main/index.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Main(){
     return(
-        <View style={{flex:2,backgroundColor:"silver",padding:10,}}>
+        <View style={{flex:1,backgroundColor:"silver",padding:10,}}>
             <View style={{backgroundColor:"white",height:"100%"}}> 
-                <Text style={{color:"blue"}}>Ola</Text>
+                <Text>
+                <Text style={{color:"blue", fontSize: 27, textAlign: "center"}}>Somos a Dog Especial. O app </Text>
+                <Text style={{color:"red", fontSize:32}}>mais</Text>
+                <Text style={{color:"blue", fontSize: 27, textAlign: "center"}}> amado pelos pets !</Text>
+                </Text>
             </View>
+            <View style={{flex:1,backgroundColor:"silver",padding:10,}}>
+            <Image source={require("../../../../assets/dog-rain.jpg" )}style={{width:340, height:100, alignContent:"center"}}/>
+            </View >
         </View>
     )
-}
+} 
